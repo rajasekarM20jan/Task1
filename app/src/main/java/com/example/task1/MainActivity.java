4ac077ded5f01a231bc65afd1cff0cd083e7b2c5
Temp-user4 <rs20jan1998s@gmail.com>
2022-11-28 19:46:00 +0530

    init

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.task1;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.BlurMaskFilter;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.text.format.Formatter;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,28 +38,55 @@ import android.widget.PopupWindow;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import adapter.CustomAdapter;
 import adapter.CustomComparisonAdapter;
 import model.ComparisonListModel;
 import model.ListModel;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private static String UniqueID;
+    private static String Latitude;
+    private static String Longitude;
+    private static String Address1;
+    LocationManager locationManager;
     //declaration of variables and layout fields
     TextView comparePlans,didNotReceive,resendOtp,timerTextView;
     ListView listView;
     ImageView close;
     GridView gridView;
     ConstraintLayout blur;
+    String resultData;
+    String[] coverageArray;
     Spinner coverageSpinner,policyTermSpinner,paymentSpinner;
 
-
+    int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 0;
     ArrayList<ComparisonListModel> comparisonList;
     ArrayList<ListModel> list;
 
@@ -52,17 +95,72 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //initialization of variables and layout fields
-        listView=findViewById(R.id.listView1);
-        comparePlans=findViewById(R.id.comparePlans);
-        blur=findViewById(R.id.blurLayout);
-        coverageSpinner=findViewById(R.id.coverageSpinner);
-        policyTermSpinner=findViewById(R.id.policyTermSpinner);
-        paymentSpinner=findViewById(R.id.paymentTypeSpinner);
-        list=new ArrayList<>();
-        comparisonList=new ArrayList<>();
+        listView = findViewById(R.id.listView1);
+        comparePlans = findViewById(R.id.comparePlans);
+        blur = findViewById(R.id.blurLayout);
+        coverageSpinner = findViewById(R.id.coverageSpinner);
+        policyTermSpinner = findViewById(R.id.policyTermSpinner);
+        paymentSpinner = findViewById(R.id.paymentTypeSpinner);
+        list = new ArrayList<>();
+        comparisonList = new ArrayList<>();
         callData();
         getOTP();
 
+
+
+        coverageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                int index=i;
+                String coverageAmount= coverageArray[i] ;
+                System.out.println("value : "+coverageAmount);
+                getLocation();
+                String Data=InsertMobileparameters(MainActivity.this);
+                System.out.println("1234567890 "+Data);
+                getCoverage(Data);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
+
+    }
+
+    private void getCoverage(String data) {
+        try{
+            OkHttpClient client=new OkHttpClient();
+            String url=getString(R.string.base_url)+"/Coverage/GetAllCoverageAmount";
+
+            RequestBody rb= RequestBody.create(MediaType.parse("application/json; charset=utf-8"),data);
+
+            Request request = new Request.Builder()
+                    .url("https://uat-integrationportal.insure.digital/api/v1/ip/ti/Coverage/GetAllCoverageAmount")
+                    .post(rb)
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                    System.out.println("My Response : "+response);
+                }
+            });
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     //Method for getting OTP Pop up
@@ -194,7 +292,7 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-            String[] coverageArray = new String[]{getString(R.string.million_10), getString(R.string.million_5), getString(R.string.million_3)};
+            coverageArray = new String[]{getString(R.string.million_10), getString(R.string.million_5), getString(R.string.million_3)};
 
             ArrayAdapter arrayAdapter1 = new ArrayAdapter(MainActivity.this, R.layout.custom_spinner_layout, coverageArray);
             try {
@@ -267,5 +365,145 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         finishAffinity();
+    }
+
+
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5, this);
+        } catch (SecurityException ex) {
+            ex.printStackTrace();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        //Toast.makeText(getActivity(), "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        //Toast.makeText(getActivity(), "GPS and Internet enabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLocationChanged(final Location location) {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                Geocoder geocoder;
+                List<Address> addresses;
+                geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                System.out.println("latlng : "+latitude+","+longitude);
+                SharedPreferences sharedpreferences = getSharedPreferences("LocationPref", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("Latitude", String.valueOf(latitude));
+                editor.putString("Longitude", String.valueOf(longitude));
+                editor.commit();
+
+                SharedPreferences sp=getSharedPreferences("LocationPref",MODE_PRIVATE);
+                Latitude=sp.getString("Latitude",null);
+                Longitude=sp.getString("Longitude",null);
+                System.out.println("latlng2 : "+Latitude+","+Longitude);
+                try {
+
+                    addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+                    if (addresses != null && addresses.size() > 0) {
+                        Address address = addresses.get(0);
+                        String addressDet = address.getAddressLine(0);
+                        SharedPreferences locashared = getSharedPreferences("LocationCurrent", MODE_PRIVATE);
+                        SharedPreferences.Editor editorloca = locashared.edit();
+                        editorloca.putString(Address1, addressDet);
+                        editorloca.commit();
+
+                    }
+
+                } catch (IOException ex) {
+                    // TODO Auto-generated catch block
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+
+    public static String InsertMobileparameters(Context context) {
+        try
+        {
+            String rooteddevice;
+            if(RootUtil.isDeviceRooted() == true)
+            {
+                rooteddevice = "1";
+            }
+            else
+            {
+                rooteddevice = "0";
+            }
+
+            String androidOS = Build.VERSION.RELEASE;
+            String model = Build.MANUFACTURER + " - " + Build.MODEL;
+            SharedPreferences uniquePref = context.getSharedPreferences("Uniquepref", MODE_PRIVATE);
+            final String uniqueidval = uniquePref.getString("imei", null);
+            final String address1 = Latitude+","+Longitude;
+            WifiManager wm = (WifiManager) context.getApplicationContext().getSystemService(WIFI_SERVICE);
+            String ipaddress = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+            JsonObject mobileparamters = new JsonObject();
+            mobileparamters.addProperty("imeino1", uniqueidval);
+            mobileparamters.addProperty("imeino2", uniqueidval);
+            mobileparamters.addProperty("timezone", TimeZone.getDefault().getDisplayName());
+            mobileparamters.addProperty("currentdatetime", java.text.DateFormat.getDateTimeInstance().format(new Date()));
+            mobileparamters.addProperty("Address", address1);
+            mobileparamters.addProperty("latitude", Latitude);
+            mobileparamters.addProperty("longitude", Longitude);
+            mobileparamters.addProperty("IpAddress", ipaddress);
+            mobileparamters.addProperty("mobileType", "Android");
+            mobileparamters.addProperty("fireBaseuserid", "123456");
+            mobileparamters.addProperty("mobileModel", model);
+            mobileparamters.addProperty("mobileOSVersion", androidOS);
+            mobileparamters.addProperty("appVersion", "1.0.6");
+            mobileparamters.addProperty("IsJailBroken", rooteddevice);
+            System.out.println(mobileparamters);
+            String insertmobileString = mobileparamters.toString();
+//            encryptedSHA = "";
+//            String sourceStr = uniqueidval + ipaddress;
+//            try {
+//                encryptedSHA = AESUtils.encrypt(sourceStr);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+            return insertmobileString;
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return "";
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
